d1cc28c739f8190b5f9ba183d4437e63ffd6108b
刘建东 <979239787@qq.com>
2023-03-08 21:30:41 +0800

    测试上传

---
+++ src/main/java/com/ljd/double12backend/vo/AtomicInfo.java
@@ -0,0 +1,103 @@
+package com.ljd.double12backend.vo;
+
+public class AtomicInfo {
+    private Long pv;
+
+    private Integer uv;
+
+    private Long allInfoNum;
+
+    private Integer userNum;
+
+    private Integer itemNum;
+
+    private Integer itemCategoryNum;
+
+    private Long browseNum;
+
+    private Integer collectNum;
+
+    private Integer cartNum;
+
+    private Integer buyNum;
+
+    public Long getPv() {
+        return pv;
+    }
+
+    public void setPv(Long pv) {
+        this.pv = pv;
+    }
+
+    public Integer getUv() {
+        return uv;
+    }
+
+    public void setUv(Integer uv) {
+        this.uv = uv;
+    }
+
+    public Long getAllInfoNum() {
+        return allInfoNum;
+    }
+
+    public void setAllInfoNum(Long allInfoNum) {
+        this.allInfoNum = allInfoNum;
+    }
+
+    public Integer getUserNum() {
+        return userNum;
+    }
+
+    public void setUserNum(Integer userNum) {
+        this.userNum = userNum;
+    }
+
+    public Integer getItemNum() {
+        return itemNum;
+    }
+
+    public void setItemNum(Integer itemNum) {
+        this.itemNum = itemNum;
+    }
+
+    public Integer getItemCategoryNum() {
+        return itemCategoryNum;
+    }
+
+    public void setItemCategoryNum(Integer itemCategoryNum) {
+        this.itemCategoryNum = itemCategoryNum;
+    }
+
+    public Long getBrowseNum() {
+        return browseNum;
+    }
+
+    public void setBrowseNum(Long browseNum) {
+        this.browseNum = browseNum;
+    }
+
+    public Integer getCollectNum() {
+        return collectNum;
+    }
+
+    public void setCollectNum(Integer collectNum) {
+        this.collectNum = collectNum;
+    }
+
+    public Integer getCartNum() {
+        return cartNum;
+    }
+
+    public void setCartNum(Integer cartNum) {
+        this.cartNum = cartNum;
+    }
+
+    public Integer getBuyNum() {
+        return buyNum;
+    }
+
+    public void setBuyNum(Integer buyNum) {
+        this.buyNum = buyNum;
+    }
+}
