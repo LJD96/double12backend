a05b93b6d7051d9e5260cc8488f5c95d140cbad6
刘建东 <979239787@qq.com>
2023-04-28 11:01:59 +0800

    增加了一个测试读取json文件的方法

--- src/test/java/com/ljd/double12backend/MyTest.java
+++ src/test/java/com/ljd/double12backend/MyTest.java
@@ -1,8 +1,10 @@
 package com.ljd.double12backend;
 
+import com.alibaba.fastjson.JSONArray;
 import com.ljd.double12backend.utils.HttpUtil;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -95,6 +97,29 @@ public class MyTest {
 
         for (Future<String> result : results) {
             result.get();
+        }
+    }
+
+    @Test
+    public void testReadJson() throws FileNotFoundException {
+        String path = "E:\\Note System\\obsidian\\主仓库\\项目\\斑马阶段\\数据治理\\fromre.json";
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+
+        String jsonString = stringBuilder.toString();
+        if (jsonString != null) {
+            JSONArray jsonArray = JSONArray.parseArray(jsonString);
+            int totalItems = jsonArray.size();
+            System.out.println("Total items in the JSON array: " + totalItems);
+        } else {
+            System.out.println("Could not read the JSON file.");
         }
     }
 }
